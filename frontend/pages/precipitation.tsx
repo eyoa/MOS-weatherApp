0db2257847cee18c012f9903e8aa77e2516e1f48
eyoa <yoaevelyn@gmail.com>
2021-09-10 22:59:40 -0400

    display weekly precipitation basic data

--- frontend/pages/precipitation.tsx
+++ frontend/pages/precipitation.tsx
@@ -1,30 +1,38 @@
-// https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
 const apiKey = process.env.NEXT_PUBLIC_API_KEY;
-const query = 'Kansas';
-const lat='33.44'
-const lon='-94.04'
+const lat = '33.44';
+const lon = '-94.04';
 
 const defaultEndpoint = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}`;
 
 export async function getServerSideProps() {
-    const res = await fetch(defaultEndpoint);
-    const data = await res.json();
-    return {
-      props: {
-        data,
-      },
-    };
-  }
+  const res = await fetch(defaultEndpoint);
+  const data = await res.json();
+  return {
+    props: {
+      data
+    }
+  };
+}
 
+export default function precipitation({ data }) {
+  return (
+    <>
+      <h1>Precipitation Page</h1>
+      <h2>This Week</h2>
+      {data.daily.map((day, index) => {
+        if (index > 6) return;
+        const date = new Date(day.dt * 1000).toLocaleDateString('en-US', {
+          weekday: 'short'
+        });
+        return (
+          <tr>
+            <td>{date}</td>
+            <td>{Math.trunc(day.pop * 100)}%</td>
+          </tr>
+        );
+      })}
 
-
-export default function precipitation({data}) {
-    const precipitations = data.daily.map(day => day.pop)
-    console.log(data.daily)
-    return <>
-        <div>Precipitation Page</div>
-        <div>{JSON.stringify(precipitations)}</div>
-        
+      <h2>This month</h2>
     </>
-    
+  );
 }
