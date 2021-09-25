108ce38bb65d5d83e59da744a70aa463986dd2c3
eyoa <yoaevelyn@gmail.com>
2021-09-25 12:22:57 -0400

    add margins and app container

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -25,30 +25,37 @@ export async function getServerSideProps() {
 const Home: NextPage = ({ data }) => {
   return (
     <WeatherContext.Provider value={data}>
-      <div className={styles.wrap}>
-        <div className={styles.slider}>
-          <div className={styles.slide} id='landing'>
-            <h1>Landing</h1>
-          </div>
-
-          <div className={styles.slide} id='Today'>
-            <Today />
-          </div>
-
-          <div className={styles.slide} id='Forecast'>
-            <Forecast />
-          </div>
-
-          <div className={styles.slide} id='Precipitation'>
-            <Precipitation />
-          </div>
+      <div className={styles.app}>
+        <div className={styles.header}>
+          <div>Last updated: 10:41AM</div>
+          <h1>Location</h1>
         </div>
 
-        <div className={styles.links}>
-          <a href='#landing'>O</a>
-          <a href='#Today'>O</a>
-          <a href='#Forecast'>O</a>
-          <a href='#Precipitation'>O</a>
+        <div className={styles.wrap}>
+          <div className={styles.slider}>
+            <div className={styles.slide} id='Landing'>
+              <h1>Landing</h1>
+            </div>
+
+            <div className={styles.slide} id='Today'>
+              <Today />
+            </div>
+
+            <div className={styles.slide} id='Forecast'>
+              <Forecast />
+            </div>
+
+            <div className={styles.slide} id='Precipitation'>
+              <Precipitation />
+            </div>
+          </div>
+
+          <div className={styles.links}>
+            <a href='#Landing'>O</a>
+            <a href='#Today'>O</a>
+            <a href='#Forecast'>O</a>
+            <a href='#Precipitation'>O</a>
+          </div>
         </div>
       </div>
     </WeatherContext.Provider>
