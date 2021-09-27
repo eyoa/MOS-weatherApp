281a4922c4da390443622a903745231da3938371
Aidan Torrence <aidan.torrence@gmail.com>
2021-09-26 20:59:34 -0400

    convert api call to use effect, add temp conversion util

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,5 +1,7 @@
 import type { NextPage } from "next"
+import {useState,useEffect} from 'react'
 import styles from '../styles/Home.module.css';
+import axios from 'axios'
 
 import Today from "../components/Today"
 import Precipitation from "../components/Precipitation"
@@ -7,20 +9,22 @@ import Forecast from "../components/forecast/Forecast"
 import { WeatherContext } from "../components/contexts/WeatherContext"
 
 const apiKey = process.env.NEXT_PUBLIC_API_KEY
-const lat = "33.44"
-const lon = "-94.04"
-const defaultEndpoint = `http://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=hourly&appid=${apiKey}&units=metric`
+const defaultEndpoint = (lat, lon, apiKey) => `http://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=hourly&appid=${apiKey}&units=metric`
 
-export async function getServerSideProps() {
-  const res = await fetch(defaultEndpoint)
-  const data = await res.json()
-  return {
-    props: {
-      data
-    }
-  }
+async function fetchData (pos, setData) {
+  const {data} = await axios.get(defaultEndpoint(pos.coords.latitude, pos.coords.longitude, apiKey))
+  setData(data)
+  console.log(data)
 }
-const Home: NextPage = ({ data }) => {
+
+const Home: NextPage = () => {
+  const [displaySearchbar, setDisplaySearchbar] = useState(false)
+  const [data, setData] = useState({})
+
+  useEffect ( () => {
+    navigator.geolocation.getCurrentPosition(pos=>fetchData(pos,setData), ()=>console.log('fail'))
+  },[])
+
   return (
     <WeatherContext.Provider value={data}>
       <div className={styles.app}>
@@ -33,18 +37,19 @@ const Home: NextPage = ({ data }) => {
             <div className={styles.slide} id='Landing'>
               <h1>Landing</h1>
             </div>
+            {Object.keys(data).length && <>
+              <div className={styles.slide} id='Today'>
+                <Today />
+              </div>
 
-            <div className={styles.slide} id='Today'>
-              <Today />
-            </div>
+              <div className={styles.slide} id='Forecast'>
+                <Forecast />
+              </div>
 
-            <div className={styles.slide} id='Forecast'>
-              <Forecast />
-            </div>
-
-            <div className={styles.slide} id='Precipitation'>
-              <Precipitation />
-            </div>
+              <div className={styles.slide} id='Precipitation'>
+                <Precipitation />
+              </div>
+            </>}
           </div>
           <div className={styles.links}>
             <a href='#Landing'>O</a>
